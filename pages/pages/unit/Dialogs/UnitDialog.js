98516b911c318b134a963ffbffd2c0a3a536cdb7
srv-cashpay <aseprayana@outlook.com>
2025-01-29 16:42:57 +0700

    Update UnitDialog.js

--- pages/pages/unit/Dialogs/UnitDialog.js
+++ pages/pages/unit/Dialogs/UnitDialog.js
@@ -7,7 +7,7 @@ const UnitDialog = ({ visible, onHide, unitData }) => {
     return (
         <Dialog visible={visible} style={{ width: '50vw' }} header="Daftar Unit" modal onHide={onHide}>
             {/* Check if unitData is an array and contains rows */}
-            <DataTable value={unitData?.data?.rows || []} responsiveLayout="scroll">
+            <DataTable value={unitData.data.rows || []} responsiveLayout="scroll">
                 <Column field="id" header="ID" />
                 <Column field="unit_name" header="Nama Unit" />
             </DataTable>
