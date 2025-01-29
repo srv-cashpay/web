3368557232d33eefeea0abac3fa0a7d61b1b101b
srv-cashpay <aseprayana@outlook.com>
2025-01-29 16:38:55 +0700

    Update CategoryDialog.js

--- pages/pages/inventory/Dialogs/CategoryDialog.js
+++ pages/pages/inventory/Dialogs/CategoryDialog.js
@@ -7,7 +7,7 @@ const CategoryDialog = ({ visible, onHide, categoryData }) => {
     return (
         <Dialog visible={visible} style={{ width: '50vw' }} header="Daftar Category" modal onHide={onHide}>
             {/* Check if categoryData is an array and contains rows */}
-            <DataTable value={categoryData?.data?.rows || []} responsiveLayout="scroll">
+            <DataTable value={categoryData.data.rows || []} responsiveLayout="scroll">
                 <Column field="id" header="ID" />
                 <Column field="category_name" header="Nama Category" />
             </DataTable>
