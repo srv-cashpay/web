e4537cf3947f9318781dfa4ce7f997f6755e7bf2
srv-cashpay <aseprayana@outlook.com>
2025-01-29 16:52:11 +0700

    Update TableDialog.js

--- pages/pages/table/Dialogs/TableDialog.js
+++ pages/pages/table/Dialogs/TableDialog.js
@@ -7,7 +7,7 @@ const TableDialog = ({ visible, onHide, tableData }) => {
     return (
         <Dialog visible={visible} style={{ width: '50vw' }} header="Daftar Table" modal onHide={onHide}>
             {/* Check if tableData is an array and contains rows */}
-            <DataTable value={tableData?.data?.rows || []} responsiveLayout="scroll">
+            <DataTable value={tableData.data.rows || []} responsiveLayout="scroll">
                 <Column field="id" header="ID" />
                 <Column field="table" header="Nama Table" />
             </DataTable>
