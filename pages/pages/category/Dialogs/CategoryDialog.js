ce641bb988b886a6fe4eb64fb71432869f552a7d
Asep rayana <asep.rayana09@gmail.com>
2025-01-30 08:25:00 +0700

    resolve

--- pages/pages/category/Dialogs/CategoryDialog.js
+++ pages/pages/category/Dialogs/CategoryDialog.js
@@ -6,12 +6,7 @@ import { Column } from 'primereact/column';
 const CategoryDialog = ({ visible, onHide, categoryData }) => {
     return (
         <Dialog visible={visible} style={{ width: '50vw' }} header="Daftar Category" modal onHide={onHide}>
-            {/* Check if categoryData is an array and contains rows */}
-<<<<<<< HEAD
-            <DataTable value={categorydata.data.rows || []} responsiveLayout="scroll">
-=======
             <DataTable value={categoryData.data.rows || []} responsiveLayout="scroll">
->>>>>>> 5d62ccc8800630bf10a0b63a1c38d4a0afe417af
                 <Column field="id" header="ID" />
                 <Column field="category_name" header="Nama Category" />
             </DataTable>
